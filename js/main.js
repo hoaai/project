821fb925ed85d79bf02c1618add76097ad994f86
Artur <arturgoldaxe@mail.ru>
2022-12-25 11:55:14 +0300

    Второй урок практики

--- js/main.js
+++ js/main.js
@@ -15,12 +15,32 @@ const personalMovieDB = {
 	privat: false,
 };
 
-const a = prompt("last watching films?", ""),
-	b = prompt("How much your opinion?", ""),
-	c = prompt("last watching films?", ""),
-	d = prompt("How much your opinion?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+	const a = prompt("last watching films?", ""),
+		b = prompt("How much your opinion?", "");
+
+	if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+		personalMovieDB.movies[a] = b;
+		console.log("Done");
+	} else {
+		console.log("error");
+		i--;
+	}
+
+}
+
+if (personalMovieDB.count < 10) {
+	console.log("Просмотрено мало фильмов");
+} else if (10 < personalMovieDB.count < 30) {
+	console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+	console.log("Вы киноман");
+} else {
+	console.log("Ошибка");
+}
+
 
 console.log(personalMovieDB);
+
+
